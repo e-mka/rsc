005035015254df13b23e74bf3d3d5706ea2726f7
Michał Koziarski <michalkoziarski@gmail.com>
2014-12-10 16:57:06 +0100

    Fixed bug with incorrect maximum value after decompression

--- rsc2ascii.m
+++ rsc2ascii.m
@@ -1,5 +1,4 @@
 function rsc2ascii(rscname, asciiname)
     [M, xmin, xmax, ymin, ymax] = rsc_file2mat(rscname);
-    M(M == Inf) = 1.70141e+038;
     rsc_grd_write(M, xmin, xmax, ymin, ymax, asciiname);
 end
--- rsc_grd_write.m
+++ rsc_grd_write.m
@@ -1,5 +1,4 @@
 function rsc_grd_write(matrix,xmin,xmax,ymin,ymax,namefile)
-
 % Function to write a MatLab matrix as a GRD file 
 %                         (Golden Software Surfer, ASCII format)
 %
@@ -15,20 +14,23 @@ function rsc_grd_write(matrix,xmin,xmax,ymin,ymax,namefile)
 % Coded by Alberto Avila Armella
 
 % Get grid dimensions
-aux=size(matrix);
-nx=aux(2);ny=aux(1);
+aux = size(matrix);
+nx = aux(2); ny = aux(1);
 
-grdfile=fopen(namefile,'w');                % Open file
-fprintf(grdfile,'%c','DSAA');               % Header code
-fprintf(grdfile,'\n %i %i',[nx ny]);        % Grid size
-fprintf(grdfile,'\n %f %f',[xmin xmax]);    % X limits
-fprintf(grdfile,'\n %f %f',[ymin ymax]);    % Y limits
-fprintf(grdfile,'\n %f %f',[min(min(matrix)) max(max(matrix))]); % Z limits
-fprintf(grdfile,'\n');
+grdfile=fopen(namefile,'w');                 % Open file
+fprintf(grdfile, '%c', 'DSAA');              % Header code
+fprintf(grdfile, '\n%i %i', [nx ny]);        % Grid size
+fprintf(grdfile, '\n%f %f', [xmin xmax]);    % X limits
+fprintf(grdfile, '\n%f %f', [ymin ymax]);    % Y limits
+fprintf(grdfile, '\n%f %f', [min(min(matrix)) max(matrix(isfinite(matrix(:, 2)), 2))]); % Z limits
+fprintf(grdfile, '\n');
+
+matrix(matrix == Inf) = 1.70141e+038;
+
 for jj=1:ny                                 % Write matrix
     for ii=1:nx
-        fprintf(grdfile,'%g %c',matrix(jj,ii),' ');
+        fprintf(grdfile, '%g %c', matrix(jj, ii), ' ');
     end
-    fprintf(grdfile,'\n');
+    fprintf(grdfile, '\n');
 end
 fclose(grdfile);
